463b65716ded062d42bcebaf4910ab01909d11bc
Ameteo Stan <stan.ameteo@gmail.com>
2019-09-29 19:29:43 +0300

    Added project structure

---
+++ src/main/java/pcbe/stock/CLI.java
@@ -0,0 +1,7 @@
+
+package pcbe.stock;
+
+public class CLI {
+    public static void main(String[] args) {
+    }
+}
---
+++ src/test/java/pcbe/PlaceholderTest.java
@@ -0,0 +1,11 @@
+package pcbe;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlaceholderTest {
+    @Test
+    void test() {
+        assertTrue(true);
+    }
+}
