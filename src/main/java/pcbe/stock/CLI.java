0fc0bf18eee0dc80642837c2af4318811888102c
Ameteo Stan <stan.ameteo@gmail.com>
2019-10-06 14:11:01 +0300

    Use thread pool for executing clients

--- src/main/java/pcbe/stock/CLI.java
+++ src/main/java/pcbe/stock/CLI.java
@@ -1,7 +1,12 @@
 
 package pcbe.stock;
 
+import java.io.IOException;
 import java.net.URI;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+
+import pcbe.stock.client.StockClientGenerator;
 import pcbe.stock.server.StockServer;
 
 public class CLI {
@@ -9,8 +14,16 @@ public class CLI {
 
     public static void main(String[] args) throws IOException {
         var stockServer = new StockServer(SERVER_URI);
+        var clients = StockClientGenerator.generateClients();
+        var executor = Executors.newCachedThreadPool();
+        try {
+            var futures = executor.invokeAll(clients);
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
         logger.info("ENTER to stop the program.");
         while (System.in.read() != '\n');
         stockServer.shutdownNow();
+        executor.shutdownNow();
     }
 }
