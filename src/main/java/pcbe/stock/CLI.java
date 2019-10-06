e9845357e75c83c09800414c7f6f3ca864d88dde
Ameteo Stan <stan.ameteo@gmail.com>
2019-10-06 14:02:47 +0300

    Start http server in cli

--- src/main/java/pcbe/stock/CLI.java
+++ src/main/java/pcbe/stock/CLI.java
@@ -1,7 +1,16 @@
 
 package pcbe.stock;
 
+import java.net.URI;
+import pcbe.stock.server.StockServer;
+
 public class CLI {
-    public static void main(String[] args) {
+    private static final URI SERVER_URI = URI.create("http://localhost:8080/stock/");
+
+    public static void main(String[] args) throws IOException {
+        var stockServer = new StockServer(SERVER_URI);
+        logger.info("ENTER to stop the program.");
+        while (System.in.read() != '\n');
+        stockServer.shutdownNow();
     }
 }
