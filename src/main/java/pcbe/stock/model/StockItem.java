3f8fc2ab34e28eefe36d5bb3da83f82e931902ca
Ameteo Stan <stan.ameteo@gmail.com>
2019-11-17 16:01:23 +0200

    Initial implementation with full functionality

--- src/main/java/pcbe/stock/model/StockItem.java
+++ src/main/java/pcbe/stock/model/StockItem.java
@@ -2,18 +2,27 @@ package pcbe.stock.model;
 
 import java.util.UUID;
 
-public class StockItem {
-	private UUID id = UUID.randomUUID();
+public abstract class StockItem {
+	private UUID id;
 	private UUID clientId;
 	private String company;
 	private int shares;
 	private double price;
 
-	public StockItem(UUID clientId, String company, int shares, double price) {
+	private StockItem(UUID id, UUID clientId, String company, int shares, double price) {
+		this.id = id;
 		this.clientId = clientId;
 		this.company = company;
 		this.shares = shares;
 		this.price = price;
+	}
+
+	protected StockItem(UUID clientId, String company, int shares, double price) {
+		this(UUID.randomUUID(), clientId, company, shares, price);
+	}
+
+	protected StockItem(StockItem stockItem) {
+		this(stockItem.id, stockItem.clientId, stockItem.company, stockItem.shares, stockItem.price);
 	}
 
 	public UUID getId() {
@@ -44,17 +53,58 @@ public class StockItem {
 		this.price = price;
 	}
 
-	public class Offer extends StockItem {
+	public static class Offer extends StockItem {
 		public Offer(UUID clientId, String company, int shares, double price) {
 			super(clientId, company, shares, price);
-			
+		}
+
+		public Offer(Offer offer) {
+			super(offer);
 		}
 	}
 
-	public class Demand extends StockItem {
+	public static class Demand extends StockItem {
 		public Demand(UUID clientId, String company, int shares, double price) {
 			super(clientId, company, shares, price);
-			
 		}
+
+		public Demand(Demand demand) {
+			super(demand);
+		}
+	}
+
+	/**
+	 * Checks if <code>this</code> and <code>other</code> 
+	 * are matching. Two <code>StockItem</code>s match if:
+	 * <ul>
+	 * 	<li>they have a different <code>clientId</code>
+	 * 	<li>they have the same <code>company</code>
+	 * 	<li>they have the same <code>price</code>
+	 * </ul>
+	 */
+	public boolean matches(StockItem other) {
+		return !clientId.equals(other.clientId)
+			&& company.equals(other.company)
+			&& price == other.price;
+	}
+
+	@Override
+	public int hashCode() {
+		return id.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null || getClass() != obj.getClass())
+			return false;
+		return StockItem.class.cast(obj).id.equals(id);
+	}
+
+	@Override
+	public String toString() {
+		return "StockItem [clientId=" + clientId + ", company=" + company + ", id=" + id + ", price=" + price
+				+ ", shares=" + shares + "]";
 	}
 }
--- src/main/java/pcbe/stock/server/StockServer.java
+++ src/main/java/pcbe/stock/server/StockServer.java
@@ -1,44 +1,86 @@
 package pcbe.stock.server;
 
-import java.util.UUID;
+import static java.util.Collections.synchronizedMap;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import java.util.logging.Logger;
+
+import pcbe.log.LogManager;
 import pcbe.stock.client.StockClient;
 import pcbe.stock.model.Response;
+import pcbe.stock.model.StockItem.Offer;
+import pcbe.stock.model.StockItem.Demand;
 
 public class StockServer {
 
+	private static final Logger logger = LogManager.getServerLogger();
 	private final StockService stockService = StockService.getDefault();
+	private Map<UUID, StockClient> clients = synchronizedMap(new HashMap<>());
 
 	public Response register(StockClient stockClient) {
-		return stockService.register(stockClient);
+		if (clients.containsKey(stockClient.getId()))
+			return Response.alreadyRegistered();
+		clients.put(stockClient.getId(), stockClient);
+		stockService.configureNotifiers(stockClient.getId(), stockClient.getNotifiers());
+		logger.info("Client " + stockClient.getId() + " registered successfully.");
+		return Response.registeredSuccessfully();
 	}
 
 	public Response offerShares(UUID clientId, String company, int shares, double price) {
-		return null;
+		if (!clients.containsKey(clientId))
+			return Response.notRegistered();
+		var offer = new Offer(clientId, company, shares, price);
+		stockService.addOffer(offer);
+		return Response.created(offer.getId());
 	}
 
-
 	public Response demandShares(UUID clientId, String company, int shares, double price) {
-		return null;
+		if (!clients.containsKey(clientId))
+			return Response.notRegistered();
+		var demand = new Demand(clientId, company, shares, price);
+		stockService.addDemand(demand);
+		return Response.created(demand.getId());
 	}
 
 	public Response changeOffer(UUID clientId, UUID offerId, int newShares, double newPrice) {
-		return null;
+		if (!clients.containsKey(clientId))
+			return Response.notRegistered();
+		try {
+			stockService.changeOffer(offerId, newShares, newPrice);
+		} catch (AlreadyInTransactionException e) {
+			return Response.ongoingTransaction();
+		}
+		return Response.changed();
 	}
 
 	public Response changeDemand(UUID clientId, UUID demandId, int newShares, double newPrice) {
-		return null;
+		if (!clients.containsKey(clientId))
+			return Response.notRegistered();
+		try {
+			stockService.changeDemand(demandId, newShares, newPrice);
+		} catch (AlreadyInTransactionException e) {
+			return Response.ongoingTransaction();
+		}
+		return Response.changed();
 	}
 
 	public Response getOffers(UUID clientId) {
-		return null;
+		return clients.containsKey(clientId)
+			? Response.offers(stockService.getOffers())
+			: Response.notRegistered();
 	}
 
 	public Response getDemands(UUID clientId) {
-		return null;
+		return clients.containsKey(clientId)
+			? Response.demands(stockService.getDemands())
+			: Response.notRegistered();
 	}
 
 	public Response getTransactionHistory(UUID clientId) {
-		return null;
+		return clients.containsKey(clientId)
+			? Response.transactions(stockService.getTransactions())
+			: Response.notRegistered();
 	}
 }
